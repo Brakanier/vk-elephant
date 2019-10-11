a65db5895947538a25073b185036b40a32e2c562
Vlasov Alexander <18brakanier.forest@gmail.com>
2019-10-11 21:28:55 +0300

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover">
+			<meta name="theme-color" content="#000000">
+			<meta content="IE=Edge" http-equiv="X-UA-Compatible">
+            <title>Купи Слона!</title>
+            <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+            <div id="root"></div>
+            <script src="{{mix('js/index.js')}}" ></script>
+	</body>
+</html>
